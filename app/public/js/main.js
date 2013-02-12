ac11468fe7a466253075380ffdb4ff36ca028dbc
Duda Dornelles <dudassdornelles@gmail.com>
2013-02-12 18:44:54 -0500

    shaking on error

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,3 +1,13 @@
+jQuery.fn.shake = function() {
+  this.each(function(i) {
+    $(this).css({ "position": "relative" });
+    for (var x = 1; x <= 2; x++) {
+      $(this).animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
+    }
+  });
+  return this;
+};
+
 $(function () {
   $("#content").load("/quiz", bindFaces);
 });
@@ -9,9 +19,14 @@ function bindFaces() {
       type: "POST",
       url: "/guess",
       data: $("#guess_form").serialize(),
-      success: function (data) {
-        $("#content").html(data);
-        bindFaces();
+      success: function (data, status, request) {
+        var correct = request.getResponseHeader("correctnes");
+        if (correct === "y") {
+          $("#content").html(data);
+          bindFaces();
+        } else {
+          $("#content").shake();
+        }
       }
     });
   });

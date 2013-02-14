79a5b4b2c9b18fe9c07790d4c618ae48259d738d
Bruno Trecenti <btrecent@thoughtworks.com>
2013-02-14 11:40:25 -0200

    removed the double quotes, just because I don't like them

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,6 @@
 jQuery.fn.shake = function() {
   this.each(function(i) {
-    $(this).css({ "position": "relative" });
+    $(this).css({ 'position': 'relative' });
     for (var x = 1; x <= 2; x++) {
       $(this).animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
     }
@@ -9,29 +9,29 @@ jQuery.fn.shake = function() {
 };
 
 $(function () {
-  $("#content").load("/quiz", bindFaces);
+  $('#content').load('/quiz', bindFaces);
 });
 
 function bindFaces() {
-  $(".tw_face").click(function (el) {
+  $('.tw_face').click(function (el) {
 
     element = el.toElement;
-    clickEvent = jQuery._data(element, "events").click[0];
+    clickEvent = jQuery._data(element, 'events').click[0];
     $(element).unbind();
 
-    $("#guess").attr("value", element.id);
+    $('#guess').attr('value', element.id);
     $.ajax({
-      type: "POST",
+      type: 'POST',
       async: false,
-      url: "/guess",
-      data: $("#guess_form").serialize(),
+      url: '/guess',
+      data: $('#guess_form').serialize(),
       success: function (data, status, request) {
-        var correct = request.getResponseHeader("correctnes");
-        if (correct === "y") { 
-          $("#content").html(data); 
+        var correct = request.getResponseHeader('correctnes');
+        if (correct === 'y') {
+          $('#content').html(data);
           bindFaces();
         } else {
-          $("#content").shake();
+          $('#content').shake();
           $(element).click(clickEvent.handler);
         }
       }
